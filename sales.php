faf3cfa13624b60ac5fa75d83b51848a567f7485
manojjonam <Manoj.jonam.boxer@gmail.com>
2022-04-13 10:29:15 +0530

    testimonial FSD

--- sales.php
+++ sales.php
@@ -389,7 +389,7 @@ include_once("includes/header.php");
 															<div class="bbt">
 																	<div class="row">
 																			<div class="col-xl-12">
-																					<img src="images/Testimonials/Selvakumar.jpg" class="img-flui w-100" alt="" title="">
+																					<img src="images/Testimonials/Selvakumar1.jpg" class="img-flui w-100" alt="" title="">
 																			</div>
 																			<div class="col-xl-12 ps-4 pe-4">
 																					<p class="school-para pt-4">"It’s been 9 months since I joined skill safari and I started to learn full-stack web development course.
@@ -412,7 +412,7 @@ Now I have a good exposure on building a website and knowledge over full stack t
 															<div class="bbt">
 																	<div class="row">
 																			<div class="col-xl-12">
-																					<img src="images/img.jpg" class="img-flui w-100" alt="" title="">
+																					<img src="images/Testimonials/krishnakumar1.jpg" class="img-flui w-100" alt="" title="">
 																			</div>
 																			<div class="col-xl-12 ps-4 pe-4">
 																					<p class="school-para pt-4">"I was amazed with the kinds of technology that is out there in the market but don't know how to use any of them .That is the point where I got a call from Skill Safari about the different courses they are offering. From the first call on December 2021 to the last call we had couple of days before, the team was extremely supportive on each queries I had, including both technical and non technical. I joined at Skill safari on January 2022 while working for another company. I was in a tight schedule trying hard to get enough time to balance my work and studies. The trainers who are very well experienced and cooperative along with supporting staff, made a lot of things easier in the journey." </p>
@@ -434,7 +434,7 @@ Now I have a good exposure on building a website and knowledge over full stack t
 															<div class="bbt">
 																	<div class="row">
 																			<div class="col-xl-12">
-																					<img src="images/img.jpg" class="img-flui w-100" alt="" title="">
+																					<img src="images/Testimonials/Aniket1.jpg" class="img-flui w-100" alt="" title="">
 																			</div>
 																			<div class="col-xl-12 ps-4 pe-4">
 																					<p class="school-para pt-4">"Skillsafari is an awesome skill development platform. I have learned lots of things, not only technical skills, but also real life things. All teams are very helpful in any situation. Thank you, dear skillsafari team for developing my career life..." </p>
@@ -456,10 +456,10 @@ Now I have a good exposure on building a website and knowledge over full stack t
 															<div class="bbt">
 																	<div class="row">
 																			<div class="col-xl-12">
-																					<img src="images/img.jpg" class="img-flui w-100" alt="" title="">
+																					<img src="images/Testimonials/Punit1.jpg" class="img-flui w-100" alt="" title="">
 																			</div>
 																			<div class="col-xl-12 ps-4 pe-4">
-																					<p class="school-para pt-4">"The teaching from the beginning was carried out in the most professional and friendly way. The trainers were very involved in helping me out with doubts at every time. They also acknowledged my work and helped me improve my skills. On the whole Great Team, I feel great satisfaction in learning under you guys and glad that every penny that I spent is worth the knowledge that you guys provided me. Its completely rare to have friendly trainers,that's the most important factor that you guys excelled in" </p>
+																					<p class="school-para pt-4">"I have got opportunity to do a course with skill safari and through that i enhanced my skill and got placed in several company." </p>
 																					<div class="starrating pt-1">
 																							<span><i class="fas fa-star"></i></span>
 																							<span><i class="fas fa-star"></i></span>
@@ -468,107 +468,13 @@ Now I have a good exposure on building a website and knowledge over full stack t
 																							<span><i class="fas fa-star-half"></i> </span>
 																							<span class="rating">4.5/5</span>
 																					</div>
-																					<h6 class="studentname pt-3 mb-0">Dharshini</h6>
+																					<h6 class="studentname pt-3 mb-0">Punit Kumar Mishra</h6>
 																					<!-- <p class="studentdest pt-1">TCS, Kochi</p> -->
 																			</div>
 																	</div>
 															</div>
 													</div>
-													<div class="item bg-white mt-3 pb-5 ps-md-2 pe-md-2">
-															<div class="bbt">
-																	<div class="row">
-																			<div class="col-xl-12">
-																					<img src="images/img.jpg" class="img-flui w-100" alt="" title="">
-																			</div>
-																			<div class="col-xl-12 ps-4 pe-4">
-																					<p class="school-para pt-4">"The teaching from the beginning was carried out in the most professional and friendly way. The trainers were very involved in helping me out with doubts at every time. They also acknowledged my work and helped me improve my skills. On the whole Great Team, I feel great satisfaction in learning under you guys and glad that every penny that I spent is worth the knowledge that you guys provided me. Its completely rare to have friendly trainers,that's the most important factor that you guys excelled in
-The best live educational platform I have ever come across. Each and every mentor over here takes utmost care of each and every person and help in every way possible. This is the platform to learn the REAL-TIME applications of the subject. I really feel overwhelmed and so grateful being one of the students of Skill Safari. I am eagerly looking forward to learning much more courses from this platform. Thank you SkillSafari!" </p>
-																					<div class="starrating pt-1">
-																							<span><i class="fas fa-star"></i></span>
-																							<span><i class="fas fa-star"></i></span>
-																							<span><i class="fas fa-star"></i></span>
-																							<span><i class="fas fa-star"></i></span>
-																							<span><i class="fas fa-star-half"></i> </span>
-																							<span class="rating">4.5/5</span>
-																					</div>
-																					<h6 class="studentname pt-3 mb-0">Meenaz Farheen Shaik</h6>
-																					<!-- <p class="studentdest pt-1">TCS, Kochi</p> -->
-																			</div>
-																	</div>
-															</div>
-													</div>
-													<div class="item bg-white mt-3 pb-5 ps-md-2 pe-md-2">
-															<div class="bbt">
-																	<div class="row">
-																			<div class="col-xl-12">
-																					<img src="images/img.jpg" class="img-flui w-100" alt="" title="">
-																			</div>
-																			<div class="col-xl-12 ps-4 pe-4">
-																					<p class="school-para pt-4">
-																							"I got to know about skill safari from my college mentor and I'm very thankful for that. Basically, I wasn't a computer student in my schooling and puc, and I lacked self confidence, but from this program to which I enrolled with skill safari gave me self-confidence and most importantly I gained a lot of interest in computers and forever thankful for it. I wish all the best to the skill safari team in all future endeavors." 
-																					</p>
-																					<div class="starrating pt-1">
-																							<span><i class="fas fa-star"></i></span>
-																							<span><i class="fas fa-star"></i></span>
-																							<span><i class="fas fa-star"></i></span>
-																							<span><i class="fas fa-star"></i></span>
-																							<span><i class="fas fa-star-half"></i> </span>
-																							<span class="rating">4.5/5</span>
-																					</div>
-																					<h6 class="studentname pt-3 mb-0">Niharika Kiran</h6>
-																					<!-- <p class="studentdest pt-1">TCS, Kochi</p> -->
-																			</div>
-																	</div>
-															</div>
-													</div>
-													<div class="item bg-white mt-3 pb-5 ps-md-2 pe-md-2">
-															<div class="bbt">
-																	<div class="row">
-																			<div class="col-xl-12">
-																					<img src="images/img.jpg" class="img-flui w-100" alt="" title="">
-																			</div>
-																			<div class="col-xl-12 ps-4 pe-4">
-																					<p class="school-para pt-4">
-																					"One thing that impressed me about Skill Safari is if I have a question there is always a quick response. Very well organized and conceived. By following the course, I was able to learn and build on the concepts with minimal questions or frustration. It taught me what I was looking to learn, was well organized, and well-paced. I’m already applying what I learned. Course content are Excellent, assignments that we build increase our knowledge, mentoring approach were good. The instructors did a good job of communicating and making it a more intimate arrangement.Skill Safari is very good and I think it’s a very solid Company." 
-																					</p>
-																					<div class="starrating pt-1">
-																							<span><i class="fas fa-star"></i></span>
-																							<span><i class="fas fa-star"></i></span>
-																							<span><i class="fas fa-star"></i></span>
-																							<span><i class="fas fa-star"></i></span>
-																							<span><i class="fas fa-star-half"></i> </span>
-																							<span class="rating">4.5/5</span>
-																					</div>
-																					<h6 class="studentname pt-3 mb-0">Krithi Hemanth</h6>
-																					<p class="studentdest pt-1">TCS, Kochi</p>
-																			</div>
-																	</div>
-															</div>
-													</div>
-													<div class="item bg-white mt-3 pb-5 ps-md-2 pe-md-2">
-															<div class="bbt">
-																	<div class="row">
-																			<div class="col-xl-12">
-																					<img src="images/img.jpg" class="img-flui w-100" alt="" title="">
-																			</div>
-																			<div class="col-xl-12 ps-4 pe-4">
-																					<p class="school-para pt-4">
-																							"Hi, this is Sumanth Valiveti. We had a good conversation with mentors and the teaching skills were awesome and they were allowed us to contact them for any doubt and at any time and this might won't happen many times as their teaching was very good and we understood each and ever bit. They used to teach us as we want and they made some boring topics very much interested. It will be much worth each rupee you paid and it also not cost too much. I am too much satisfied with this and the courses they provide will be so useful for our future."
-																					</p>
-																					<div class="starrating pt-1">
-																							<span><i class="fas fa-star"></i></span>
-																							<span><i class="fas fa-star"></i></span>
-																							<span><i class="fas fa-star"></i></span>
-																							<span><i class="fas fa-star"></i></span>
-																							<span><i class="fas fa-star-half"></i> </span>
-																							<span class="rating">4.5/5</span>
-																					</div>
-																					<h6 class="studentname pt-3 mb-0">Sumanth Valiveti</h6>
-																					<!-- <p class="studentdest pt-1">TCS, Kochi</p> -->
-																			</div>
-																	</div>
-															</div>
-													</div>
+													
 												</div>
 											</div>
 										</div>
