51dcadfa16c2b5800ee01e75ca94756a776e7117
kishore <kishore.sai37@gmail.com>
2022-04-16 13:13:35 +0530

    update community page

--- community.php
+++ community.php
@@ -10,253 +10,142 @@ include_once("includes/header.php");
 <section class="top-pagespace"></section>
 <!-- hire banner section started -->
 <section class="bg-hire">
-    <div class="container">
-        <div class="row pt-5 pb-md-5">
-            <div class="col-md-6 text-center text-md-start">
-                <!-- <h1 class="top-head pt-lg-5">Lorem Ipsum <br> Dolor sit amet</h1> -->
-                <p class="school-para pt-lg-5  pt-3">A vibrant community is a key to one’s overall development. Join the Discord community at Skill Safari and become part of the hundreds of activities we have for you.</p>
-                <p class="school-para pt-lg-5  pt-3">Join Skill Safari’s Discord Community and get access to all of these things:</p>
-                <ul>
-                    <li>Coding Challenges, Hackathons and Codathons</li>
-                    <li>Latest Technology News Updates</li>
-                    <li>Industry Updates</li>
-                    <li>Latest Recruitments and Internship Updates</li>
-                    <li>Skill Safari Events and Challenges</li>
-                    <li>Giveaways and Freebies for active people</li>
-                
-                </ul>
-                <div class="regbtn mb-5 mt-lg-5">
-                   <p>So what are you waiting for? </p> <a href="" class="text-decoration-none">Join Community</a>
-                </div>
-            </div>
-            <div class="col-md-6 pb-5">
-                <img src="images/ab6.jpg" class="img-fluid" alt="" title="">
-            </div>
-           
+  <div class="container">
+    <div class="row pt-5 pb-md-5 mx-auto">
+      <div class="col-md-6 pb-5 d-md-none">
+        <img src="images/ab6.jpg" class="img-fluid" alt="" title="">
+      </div>
+      <div class="col-md-6 text-start">
+        <!-- <h1 class="top-head pt-lg-5">Lorem Ipsum <br> Dolor sit amet</h1> -->
+        <p class="school-para pt-lg-5  pt-3">A vibrant community is a key to one’s overall development. Join the Discord
+          community at Skill Safari and become part of the hundreds of activities we have for you.</p>
+        <p class="school-para pt-lg-5  pt-3">Join Skill Safari’s Discord Community and get access to all of these
+          things:</p>
+        <ul>
+          <li>Coding Challenges, Hackathons and Codathons</li>
+          <li>Latest Technology News Updates</li>
+          <li>Industry Updates</li>
+          <li>Latest Recruitments and Internship Updates</li>
+          <li>Skill Safari Events and Challenges</li>
+          <li>Giveaways and Freebies for active people</li>
+        </ul>
+        <div class="regbtn mb-5 mt-lg-5 text-center">
+          <p>So what are you waiting for? </p>
+          <a href="" class="text-decoration-none">Join Community</a>
         </div>
-        <div class="row  pb-md-5">
-            <div class="col-md-6 pb-md-5">
-                <img src="images/ab7.jpg" class="img-fluid" alt="" title="">
-            </div>
-            <div class="col-md-6 text-center text-md-start">
-                <!-- <h1 class="top-head pt-lg-5 pt-3">Lorem Ipsum <br> Dolor sit amet</h1> -->
-                <p class="school-para pt-3">
-                Join us at Skill Safari as we usher in the new era of e-learning, where bookish knowledge is not the king. We are here to provide you with interesting learnable tasks and events that make you think and help you build a new and better future. <br>
-                <br>
-                Want to change the way you learn? Become a part of the Skill Safari community and become part of a new era of e-learning and growth. 
-                </p>
-                <p>“We create thinkers that have the potential to change the world. So, why wait? Join us for the ride now!”</p>
-                
-                <div class="regbtn mb-5 mt-lg-5">
-                    <a href="" class="text-decoration-none">Join Community</a>
-                </div>
-            </div>
-                  
-        </div>
+      </div>
+      <div class="col-md-6 pb-5 d-none d-md-block">
+        <img src="images/ab6.jpg" class="img-fluid" alt="" title="">
+      </div>
     </div>
+    <div class="row  pb-md-5 mx-auto">
+      <div class="col-md-6 pb-md-5">
+        <img src="images/ab7.jpg" class="img-fluid" alt="" title="">
+      </div>
+      <div class="col-md-6 text-start">
+        <!-- <h1 class="top-head pt-lg-5 pt-3">Lorem Ipsum <br> Dolor sit amet</h1> -->
+        <p class="school-para pt-3">
+          Join us at Skill Safari as we usher in the new era of e-learning, where bookish knowledge is not the king. We
+          are here to provide you with interesting learnable tasks and events that make you think and help you build a
+          new and better future. <br>
+          <br>
+          Want to change the way you learn? Become a part of the Skill Safari community and become part of a new era of
+          e-learning and growth.
+        </p>
+        <p>“We create thinkers that have the potential to change the world. So, why wait? Join us for the ride now!”</p>
+
+        <div class="regbtn mb-5 mt-lg-5 text-center">
+          <a href="" class="text-decoration-none">Join Community</a>
+        </div>
+      </div>
+
+    </div>
+  </div>
 </section>
 <!-- hire banner section ended -->
 
-<!-- events section started -->
-<section class="bg-eventssection pb-5 position-relative">
-    <div class="container">
-        <div class="row pt-lg-5">
-            <div class="col-md-12 text-center pb-5">
-                <h1 class="top-head">Ongoing Events</h1>
-            </div>
-            <div class="cir-dot-blue d-none d-lg-block">
-                <img src="images/bluecircle.png" class="img-fluid" alt="" title="">
-            </div>
-            <div class="cir-dot-yellow d-none d-lg-block">
-                <img src="images/dotyellow.png" class="img-fluid" alt="" title="">
-            </div>
-            
-            <div id="owl-events" class="owl-carousel owl-theme">
-                <div class="item bg-white ps-md-2 pe-md-2 ps-lg-2 pe-lg-2">                    
-                    <div class="event-boxlay">
-                        <div class="position-relative img-overblacklay">
-                            <img src="images/event.jpg" class="img-fluid" alt="" title="">
-                            <div class="top-small-strip">
-                                <h5 class="mb-0">15 seats left</h5>
-                            </div>
-                        </div>
-                        <div class="ps-3 pe-3 pt-2">
-                            <div class="row">
-                                <div class="col-md-8 col-8">
-                                    <h6 class="event-minisec pt-3">Next Session on 09/01/2022</h6>
-                                    <h1 class="event-boxhead pt-3">Sales <br> Workshop</h1>
-                                    <p class="school-para">Mohit Ahuja <br>Ex – Zomato, Curefit, RadYes</p>
-                                </div>
-                                <div class="col-md-4 col-4 text-end pt-3">
-                                    <div class="pricetag-strike pt-3"><strike style="color:#888;font-size:14px;">₹ 1290</strike><br>₹ 1000</div>
-                                </div>
-                            </div>
-                            
-                            <div class="row align-items-center pt-3 pb-4 text-center text-md-start">
-                                <div class="col-md-6 col-sm-6 col-12 mb-4 mb-md-0">
-                                    <div class="regbtn">
-                                        <a href="events.php" class="text-decoration-none">Know More</a>
-                                    </div>
-                                </div>
-                                <div class="col-md-6 col-sm-6 col-12">
-                                    <h6 class="event-avail">120 Students Enrolled</h6>
-                                </div>
-                            </div>
-                        </div>
-                    </div>           
-                </div>
-                <div class="item bg-white ps-md-2 pe-md-2 ps-lg-2 pe-lg-2">                    
-                    <div class="event-boxlay">
-                        <div class="position-relative img-overblacklay">
-                            <img src="images/event2.jpg" class="img-fluid" alt="" title="">
-                            <div class="top-small-strip">
-                                <h5 class="mb-0">15 seats left</h5>
-                            </div>
-                        </div>
-                        <div class="ps-3 pe-3 pt-2">
-                            <div class="row">
-                                <div class="col-md-8 col-8">
-                                    <h6 class="event-minisec pt-3">Next Session on 09/01/2022</h6>
-                                    <h1 class="event-boxhead pt-3">Sales <br> Workshop</h1>
-                                    <p class="school-para">Mohit Ahuja <br>Ex – Zomato, Curefit, RadYes</p>
-                                </div>
-                                <div class="col-md-4 col-4 text-end pt-3">
-                                    <div class="pricetag-strike pt-3"><strike style="color:#888;font-size:14px;">₹ 1290</strike><br>₹ 1000</div>
-                                </div>
-                            </div>
-                            
-                            <div class="row align-items-center pt-3 pb-4 text-center text-md-start">
-                                <div class="col-md-6 col-sm-6 col-12 mb-4 mb-md-0">
-                                    <div class="regbtn">
-                                        <a href="events.php" class="text-decoration-none">Know More</a>
-                                    </div>
-                                </div>
-                                <div class="col-md-6 col-sm-6 col-12">
-                                    <h6 class="event-avail">120 Students Enrolled</h6>
-                                </div>
-                            </div>
-                        </div>
-                    </div>           
-                </div>
-                <div class="item bg-white ps-md-2 pe-md-2 ps-lg-2 pe-lg-2">                    
-                    <div class="event-boxlay">
-                        <div class="position-relative img-overblacklay">
-                            <img src="images/event3.jpg" class="img-fluid" alt="" title="">
-                            <div class="top-small-strip">
-                                <h5 class="mb-0">15 seats left</h5>
-                            </div>
-                        </div>
-                        <div class="ps-3 pe-3 pt-2">
-                            <div class="row">
-                                <div class="col-md-8 col-8">
-                                    <h6 class="event-minisec pt-3">Next Session on 09/01/2022</h6>
-                                    <h1 class="event-boxhead pt-3">Sales <br> Workshop</h1>
-                                    <p class="school-para">Mohit Ahuja <br>Ex – Zomato, Curefit, RadYes</p>
-                                </div>
-                                <div class="col-md-4 col-4 text-end pt-3">
-                                    <div class="pricetag-strike pt-3"><strike style="color:#888;font-size:14px;">₹ 1290</strike><br>₹ 1000</div>
-                                </div>
-                            </div>
-                            
-                            <div class="row align-items-center pt-3 pb-4 text-center text-md-start">
-                                <div class="col-md-6 col-sm-6 col-12 mb-4 mb-md-0">
-                                    <div class="regbtn">
-                                        <a href="events.php" class="text-decoration-none">Know More</a>
-                                    </div>
-                                </div>
-                                <div class="col-md-6 col-sm-6 col-12">
-                                    <h6 class="event-avail">120 Students Enrolled</h6>
-                                </div>
-                            </div>
-                        </div>
-                    </div>           
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-<!-- events section ended -->
 
 <!-- community section started -->
 <section class="bg-community">
-    <div class="container">
+  <div class="container">
+    <div class="row pt-5 mx-auto">
+      <div class="col-lg-4 col-md-12">
+        <img src="images/discord.jpg" class="img-fluid w-100" alt="" title="">
+      </div>
+      <div class="col-lg-8 col-md-12">
+        <h1 class="top-head pt-4 pt-lg-0">Join Now Our <br> Discord Community</h1>
         <div class="row pt-5">
-            <div class="col-lg-4 col-md-12">
-                <img src="images/discord.jpg" class="img-fluid w-100" alt="" title="">
-            </div>
-            <div class="col-lg-8 col-md-12">
-                <h1 class="top-head pt-4 pt-lg-0">Join Now Our <br> Discord Community</h1>
-                <div class="row pt-5">
-                    <div class="col-md-6 pb-md-5">
-                        <div class="row align-items-center">
-                            <div class="col-md-3 col-sm-3 col-3 mb-3 mb-md-0">
-                                <div class="community-iconbox shadow text-center p-3">
-                                    <img src="images/c1.png" class="img-fluid w-100" alt="" title="">
-                                </div>
-                            </div>
-                            <div class="col-md-9 col-sm-9">
-                                <p class="school-para">We provide regular tasks, coding challenges, study materials etc.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6 pb-md-5">
-                        <div class="row align-items-center">
-                            <div class="col-md-3 col-sm-3 col-3 mb-3 mb-md-0">
-                                <div class="community-iconbox shadow text-center p-3">
-                                    <img src="images/c1.png" class="img-fluid w-100" alt="" title="">
-                                </div>
-                            </div>
-                            <div class="col-md-9 col-sm-9">
-                                <p class="school-para">Get the latest news about recruiters and companies 
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6 pb-md-5">
-                        <div class="row align-items-center">
-                            <div class="col-md-3 col-sm-3 col-3 mb-3 mb-md-0">
-                                <div class="community-iconbox shadow text-center p-3">
-                                    <img src="images/c1.png" class="img-fluid w-100" alt="" title="">
-                                </div>
-                            </div>
-                            <div class="col-md-9 col-sm-9">
-                                <p class="school-para">Stay in check with all the tech developments happening around you.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6 pb-md-5">
-                        <div class="row align-items-center">
-                            <div class="col-md-3 col-sm-3 col-3 mb-3 mb-md-0">
-                                <div class="community-iconbox shadow text-center p-3">
-                                    <img src="images/c1.png" class="img-fluid w-100" alt="" title="">
-                                </div>
-                            </div>
-                            <div class="col-md-9 col-sm-9">
-                                <p class="school-para">We have ebooks, giveaways and a lot more to keep your minds buzzing with ideas.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-12 text-center mt-4">
-                        <div class="discordbtn text-center">
-                        <a href="community.php" class="text-decoration-none"><img src="images/baba.png" class="img-fluid me-1"> Join Our Discord</a>
-                        </div>
-                    </div>
+          <div class="col-md-6 pb-md-5">
+            <div class="row align-items-center">
+              <div class="col-md-3 col-sm-3 col-3 mb-3 mb-md-0">
+                <div class="community-iconbox shadow text-center p-3">
+                  <img src="images/c1.png" class="img-fluid w-100" alt="" title="">
                 </div>
+              </div>
+              <div class="col-md-9 col-sm-9">
+                <p class="school-para">We provide regular tasks, coding challenges, study materials etc.
+                </p>
+              </div>
             </div>
+          </div>
+          <div class="col-md-6 pb-md-5">
+            <div class="row align-items-center">
+              <div class="col-md-3 col-sm-3 col-3 mb-3 mb-md-0">
+                <div class="community-iconbox shadow text-center p-3">
+                  <img src="images/c1.png" class="img-fluid w-100" alt="" title="">
+                </div>
+              </div>
+              <div class="col-md-9 col-sm-9">
+                <p class="school-para">Get the latest news about recruiters and companies
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 pb-md-5">
+            <div class="row align-items-center">
+              <div class="col-md-3 col-sm-3 col-3 mb-3 mb-md-0">
+                <div class="community-iconbox shadow text-center p-3">
+                  <img src="images/c1.png" class="img-fluid w-100" alt="" title="">
+                </div>
+              </div>
+              <div class="col-md-9 col-sm-9">
+                <p class="school-para">Stay in check with all the tech developments happening around you.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 pb-md-5">
+            <div class="row align-items-center">
+              <div class="col-md-3 col-sm-3 col-3 mb-3 mb-md-0">
+                <div class="community-iconbox shadow text-center p-3">
+                  <img src="images/c1.png" class="img-fluid w-100" alt="" title="">
+                </div>
+              </div>
+              <div class="col-md-9 col-sm-9">
+                <p class="school-para">We have ebooks, giveaways and a lot more to keep your minds buzzing with ideas.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-12 text-center mt-4">
+            <div class="discordbtn text-center">
+              <a href="community.php" class="text-decoration-none"><img src="images/baba.png" class="img-fluid me-1">
+                Join Our Discord</a>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </section>
 <!-- community section ended -->
 
 <section class="bg-lineball">
-    <div class="container-fluid p-0">
-        <div class="row">
-            <div class="col-md-12 pt-5 pb-5  mb-5"></div>
-        </div>
+  <div class="container-fluid p-0">
+    <div class="row">
+      <div class="col-md-12 pt-5 pb-5  mb-5"></div>
     </div>
+  </div>
 </section>
 
 
@@ -302,4 +191,3 @@ include_once("includes/header.php");
 <?php 
 include_once("includes/footer.php");
 ?>
- 
