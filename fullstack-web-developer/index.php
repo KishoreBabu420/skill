376bead5acd9819b32b86455e043532f024f11a7
kishore <kishore.sai37@gmail.com>
2022-04-26 13:06:40 +0530

    update other form

--- fullstack-web-developer/index.php
+++ fullstack-web-developer/index.php
@@ -783,10 +783,10 @@ include_once("../includes/header.php");
           <div class="sidebar">
             <div class="training-form bg-white p-md-5 p-3">
               <h2 class="trainingform-head pb-3">Request A Callback</h2>
-              <form action="https://formsubmit.co/99a7386ade92b752097e7774d17f24fc">
+              <form action="https://formsubmit.co/99a7386ade92b752097e7774d17f24fc" method="POST">
                 <div class="mb-3">
                   <label for="fullName" class="form-label">Full Name*</label>
-                  <input type="name" class="form-control" id="fullName" aria-describedby="fullNameHelp" name="fullName"
+                  <input type="name" class="form-control" id="fullName" aria-describedby="fullNameHelp" name="name"
                     placeholder="Enter Your Full Name" />
                 </div>
 
@@ -799,7 +799,7 @@ include_once("../includes/header.php");
                 <div class="mb-3">
                   <label for="mobile" class="form-label">Mobile Number*</label>
                   <input type="tel" class="form-control" id="mobile" aria-describedby="mobileHelp"
-                    placeholder="Enter Your Mobile Number" name="mobile" />
+                    placeholder="Enter Your Mobile Number" name="phone" />
                 </div>
                 <input type="hidden" name="_webhook"
                   value="https://connect.pabbly.com/workflow/sendwebhookdata/IjM1MzIxMCI_3D">
