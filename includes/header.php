cf6e63385c081762eee671a02ad8a47d5e7c1c00
kishore <kishore.sai37@gmail.com>
2022-04-28 16:01:36 +0530

    add analytics pixe

--- includes/header.php
+++ includes/header.php
@@ -26,6 +26,46 @@
   <link rel="stylesheet" type="text/css" href="<?php echo $owlCarouselCSS; ?>">
   <link rel="stylesheet" type="text/css" href="<?php echo $owlThemeCSS; ?>">
   <script src="https://kit.fontawesome.com/7096014502.js" crossorigin="anonymous"></script>
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-XS84CYLEYF"></script>
+  <script>
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag('js', new Date());
+
+  gtag('config', 'G-XS84CYLEYF');
+  </script>
+
+  <!-- Meta Pixel Code -->
+  <script>
+  ! function(f, b, e, v, n, t, s) {
+    if (f.fbq) return;
+    n = f.fbq = function() {
+      n.callMethod ?
+        n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+    };
+    if (!f._fbq) f._fbq = n;
+    n.push = n;
+    n.loaded = !0;
+    n.version = '2.0';
+    n.queue = [];
+    t = b.createElement(e);
+    t.async = !0;
+    t.src = v;
+    s = b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t, s)
+  }(window, document, 'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '190087729661951');
+  fbq('track', 'PageView');
+  </script>
+  <noscript><img height="1" width="1" style="display:none"
+      src="https://www.facebook.com/tr?id=190087729661951&ev=PageView&noscript=1" /></noscript>
+  <!-- End Meta Pixel Code -->
 </head>
 
 <body>
