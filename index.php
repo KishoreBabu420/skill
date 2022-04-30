3c1861c12492c6c8129303ad97f26beaeca4b5b9
kishore <kishore.sai37@gmail.com>
2022-04-30 17:42:09 +0530

    update change text

--- index.php
+++ index.php
@@ -14,9 +14,10 @@ include_once("includes/header.php");
   <div class="container">
     <div class="row align-items-center">
       <div class="col-md-6">
-        <h1 class="banner-head text-md-start text-center">Full Stack Developer Course with Placement Assurance
+        <h1 class="banner-head text-md-start text-center">Full Stack Developer Course with
         </h1>
-        <p class="banner-sub-head text-md-start text-center"><span class="obl-principles"></span></p>
+        <p class="banner-sub-head change-text text-md-start text-center"><span class="obl-principles"></span></p>
+        <p class="banner-sub-head text-md-start text-center"> & Placement Assurance</p>
         <div class="regbtn mt-4 text-md-start text-center">
           <button><a href="fullstack-web-developer" class="text-decoration-none text-light">Learn More</a> </button>
         </div>
