c5ce3f9f93c2bc8f0c68188e5bbc74626fb3f5be
manojjonam <Manoj.jonam.boxer@gmail.com>
2022-05-01 11:22:34 +0530

    added alt and links to patnering companies

--- includes/partners.php
+++ includes/partners.php
@@ -9,196 +9,196 @@
     <div class="owl-carousel pb-2" id="partner-carouselOne">
       <div class=" text-center">
         <div class="partner__logo-container p-2">
-          <!-- <a href="#"> -->
+          <a href="https://www.zomato.com/" target="_blank">
           <img src="https://res.cloudinary.com/skill-safari/image/upload/v1650549941/website/company-logo/3_udsdou.png"
-            alt="" class="img-fluid" loading="lazy">
-          <!-- </a> -->
+            alt="zomato" class="img-fluid" loading="lazy">
+          </a>
         </div>
       </div>
       <div class=" text-center">
         <div class="partner__logo-container p-2">
-          <!-- <a href="#"> -->
+          <a href="https://www.freshworks.com/" target="_blank">
           <img src="https://res.cloudinary.com/skill-safari/image/upload/v1650549940/website/company-logo/11_fagzql.png"
-            alt="" class="img-fluid" loading="lazy">
-          <!-- </a> -->
+            alt="Freshworks" class="img-fluid" loading="lazy">
+          </a>
         </div>
       </div>
       <div class=" text-center">
         <div class="partner__logo-container p-2">
-          <!-- <a href="#"> -->
+          <a href="https://www.redbus.in/" target="_blank">
           <img src="https://res.cloudinary.com/skill-safari/image/upload/v1650549940/website/company-logo/13_tvprkr.png"
-            alt="" class="img-fluid" loading="lazy">
-          <!-- </a> -->
+            alt="Red bus" class="img-fluid" loading="lazy">
+          </a>
         </div>
       </div>
       <div class=" text-center">
         <div class="partner__logo-container p-2">
-          <!-- <a href="#"> -->
+          <a href="https://in.bookmyshow.com/" target="_blank">
           <img src="https://res.cloudinary.com/skill-safari/image/upload/v1650549940/website/company-logo/12_hoj4gr.png"
-            alt="" class="img-fluid" loading="lazy">
-          <!-- </a> -->
+            alt="Bookmyshow" class="img-fluid" loading="lazy">
+          </a>
         </div>
       </div>
       <div class=" text-center">
         <div class="partner__logo-container p-2">
-          <!-- <a href="#"> -->
+          <a href="https://www.zoho.com/" target="_blank">
           <img src="https://res.cloudinary.com/skill-safari/image/upload/v1650549940/website/company-logo/10_qfmlvn.png"
-            alt="" class="img-fluid" loading="lazy">
-          <!-- </a> -->
+            alt="ZOHO" class="img-fluid" loading="lazy">
+          </a>
         </div>
       </div>
       <div class=" text-center">
         <div class="partner__logo-container p-2">
-          <!-- <a href="#"> -->
+          <a href="https://www.olacabs.com/" target="_blank">
           <img src="https://res.cloudinary.com/skill-safari/image/upload/v1650549939/website/company-logo/7_tece5d.png"
-            alt="" class="img-fluid" loading="lazy">
-          <!-- </a> -->
+            alt="OLA" class="img-fluid" loading="lazy">
+          </a>
         </div>
       </div>
       <div class=" text-center">
         <div class="partner__logo-container p-2">
-          <!-- <a href="#"> -->
+          <a href="https://www.dream11.com/" target="_blank">
           <img src="https://res.cloudinary.com/skill-safari/image/upload/v1650549939/website/company-logo/4_pxnskh.png"
-            alt="" class="img-fluid" loading="lazy">
-          <!-- </a> -->
+            alt="Dream 11" class="img-fluid" loading="lazy">
+          </a>
         </div>
       </div>
       <div class=" text-center">
         <div class="partner__logo-container p-2">
-          <!-- <a href="#"> -->
+          <a href="https://www.globallogic.com/in/" target="_blank">
           <img src="https://res.cloudinary.com/skill-safari/image/upload/v1650549939/website/company-logo/5_wgt0xy.png"
-            alt="" class="img-fluid" loading="lazy">
-          <!-- </a> -->
+            alt="Global Logic" class="img-fluid" loading="lazy">
+          </a>
         </div>
       </div>
       <div class=" text-center">
         <div class="partner__logo-container p-2">
-          <!-- <a href="#"> -->
+          <a href="https://www.citrix.com/" target="_blank">
           <img src="https://res.cloudinary.com/skill-safari/image/upload/v1650549939/website/company-logo/9_oiozex.png"
-            alt="" class="img-fluid" loading="lazy">
-          <!-- </a> -->
+            alt="Citrix" class="img-fluid" loading="lazy">
+          </a>
         </div>
       </div>
       <div class=" text-center">
         <div class="partner__logo-container p-2">
-          <!-- <a href="#"> -->
+          <a href="https://www.blackrock.com/" target="_blank">
           <img src="https://res.cloudinary.com/skill-safari/image/upload/v1650549939/website/company-logo/8_jdkvvy.png"
-            alt="" class="img-fluid" loading="lazy">
-          <!-- </a> -->
+            alt="Black Rock" class="img-fluid" loading="lazy">
+          </a>
         </div>
       </div>
       <div class=" text-center">
         <div class="partner__logo-container p-2">
-          <!-- <a href="#"> -->
+          <a href="https://phicode.io/" target="_blank">
           <img src="https://res.cloudinary.com/skill-safari/image/upload/v1650549940/website/company-logo/16_w1til3.png"
-            alt="" class="img-fluid" loading="lazy">
-          <!-- </a> -->
+            alt="Phicode" class="img-fluid" loading="lazy">
+          </a>
         </div>
       </div>
       <div class=" text-center">
         <div class="partner__logo-container p-2">
-          <!-- <a href="#"> -->
+          <a href="https://www.elhaa.com/" target="_blank">
           <img src="https://res.cloudinary.com/skill-safari/image/upload/v1650549940/website/company-logo/19_pwqziy.png"
-            alt="" class="img-fluid" loading="lazy">
-          <!-- </a> -->
+            alt="Elhaa Technologies" class="img-fluid" loading="lazy">
+          </a>
         </div>
       </div>
     </div>
     <div class="owl-carousel" id="partner-carouselTwo">
       <div class=" text-center">
         <div class="partner__logo-container p-2">
-          <!-- <a href="#"> -->
+          <a href="https://bewelldigital.com/" target="_blank">
           <img src="https://res.cloudinary.com/skill-safari/image/upload/v1650549940/website/company-logo/15_forjek.png"
-            alt="" class="img-fluid" loading="lazy">
-          <!-- </a> -->
+            alt="BewellDigital" class="img-fluid" loading="lazy">
+          </a>
         </div>
       </div>
       <div class=" text-center">
         <div class="partner__logo-container p-2">
-          <!-- <a href="#"> -->
+          <a href="https://alphaobs.com/index.php" target="_blank">
           <img src="https://res.cloudinary.com/skill-safari/image/upload/v1650549940/website/company-logo/14_iv1jvq.png"
-            alt="" class="img-fluid" loading="lazy">
-          <!-- </a> -->
+            alt="alpha OBS " class="img-fluid" loading="lazy">
+          </a>
         </div>
       </div>
       <div class=" text-center">
         <div class="partner__logo-container p-2">
-          <!-- <a href="#"> -->
+          <a href="#" target="_blank">
           <img src="https://res.cloudinary.com/skill-safari/image/upload/v1650549940/website/company-logo/18_nfy93y.png"
             alt="" class="img-fluid" loading="lazy">
-          <!-- </a> -->
+          </a>
         </div>
       </div>
       <div class=" text-center">
         <div class="partner__logo-container p-2">
-          <!-- <a href="#"> -->
+          <a href="https://www.dafidy.com/" target="_blank">
           <img src="https://res.cloudinary.com/skill-safari/image/upload/v1650549940/website/company-logo/20_gqwkc9.png"
-            alt="" class="img-fluid" loading="lazy">
-          <!-- </a> -->
+            alt="Dafidy" class="img-fluid" loading="lazy">
+          </a>
         </div>
       </div>
       <div class=" text-center">
         <div class="partner__logo-container p-2">
-          <!-- <a href="#"> -->
+          <a href="https://draup.com/" target="_blank">
           <img src="https://res.cloudinary.com/skill-safari/image/upload/v1650549941/website/company-logo/21_j5k5qa.png"
-            alt="" class="img-fluid" loading="lazy">
-          <!-- </a> -->
+            alt="Draup" class="img-fluid" loading="lazy">
+          </a>
         </div>
       </div>
       <div class=" text-center">
         <div class="partner__logo-container p-2">
-          <!-- <a href="#"> -->
+          <a href="https://www.gharoffice.com/" target="_blank">
           <img src="https://res.cloudinary.com/skill-safari/image/upload/v1650549941/website/company-logo/23_alrqmc.png"
-            alt="" class="img-fluid" loading="lazy">
-          <!-- </a> -->
+            alt="Gharoffice" class="img-fluid" loading="lazy">
+          </a>
         </div>
       </div>
       <div class=" text-center">
         <div class="partner__logo-container p-2">
-          <!-- <a href="#"> -->
+          <a href="http://www.finacus.co.in/" target="_blank">
           <img src="https://res.cloudinary.com/skill-safari/image/upload/v1650549941/website/company-logo/24_efmagh.png"
-            alt="" class="img-fluid" loading="lazy">
-          <!-- </a> -->
+            alt="Finacus Solutions" class="img-fluid" loading="lazy">
+          </a>
         </div>
       </div>
       <div class=" text-center">
         <div class="partner__logo-container p-2">
-          <!-- <a href="#"> -->
+          <a href="https://iwebbracket.com/" target="_blank">
           <img src="https://res.cloudinary.com/skill-safari/image/upload/v1650549941/website/company-logo/17_dncxxc.png"
-            alt="" class="img-fluid" loading="lazy">
-          <!-- </a> -->
+            alt="iWebBracket" class="img-fluid" loading="lazy">
+          </a>
         </div>
       </div>
       <div class=" text-center">
         <div class="partner__logo-container p-2">
-          <!-- <a href="#"> -->
+          <a href="http://idrive.capital/" target="_blank">
           <img src="https://res.cloudinary.com/skill-safari/image/upload/v1650549941/website/company-logo/22_o2lzul.png"
-            alt="" class="img-fluid" loading="lazy">
-          <!-- </a> -->
+            alt="idrive capital" class="img-fluid" loading="lazy">
+          </a>
         </div>
       </div>
       <div class=" text-center">
         <div class="partner__logo-container p-2">
-          <!-- <a href="#"> -->
+          <a href="https://paytm.com/" target="_blank">
           <img src="https://res.cloudinary.com/skill-safari/image/upload/v1650549941/website/company-logo/1_k6vqne.png"
-            alt="" class="img-fluid" loading="lazy">
-          <!-- </a> -->
+            alt="Paytm" class="img-fluid" loading="lazy">
+          </a>
         </div>
       </div>
       <div class=" text-center">
         <div class="partner__logo-container p-2">
-          <!-- <a href="#"> -->
+          <a href="https://vaeso.com/" target="_blank">
           <img src="https://res.cloudinary.com/skill-safari/image/upload/v1650549941/website/company-logo/25_ezgyed.png"
-            alt="" class="img-fluid" loading="lazy">
-          <!-- </a> -->
+            alt="VAESO" class="img-fluid" loading="lazy">
+          </a>
         </div>
       </div>
       <div class=" text-center">
         <div class="partner__logo-container p-2">
-          <!-- <a href="#"> -->
+          <a href="https://www.whitehatjr.com/" target="_blank">
           <img src="https://res.cloudinary.com/skill-safari/image/upload/v1650549939/website/company-logo/6_jdhues.png"
-            alt="" class="img-fluid" loading="lazy">
-          <!-- </a> -->
+            alt="whitehatjr" class="img-fluid" loading="lazy">
+          </a>
         </div>
       </div>
     </div>
