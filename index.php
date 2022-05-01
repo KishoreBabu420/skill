5cfb0d96362f1a480c1ff1dbcb10fd686466d8f1
kishore <kishore.sai37@gmail.com>
2022-05-01 11:52:53 +0530

    update header title

--- index.php
+++ index.php
@@ -17,6 +17,7 @@ include_once("includes/header.php");
         <h1 class="banner-head text-md-start text-center">Full Stack Developer Course with
         </h1>
         <p class="banner-sub-head text-md-start text-center"><span class="obl-principles change-text"></span></p>
+        <p class="banner-sub-head text-md-start text-center"> & Placement Assurance</p>
         <div class="regbtn mt-4 text-md-start text-center">
           <button><a href="fullstack-web-developer" class="text-decoration-none text-light">Learn More</a> </button>
         </div>
