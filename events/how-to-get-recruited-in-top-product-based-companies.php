18e90f720c6bdc2d423236e25c824c4163de9363
manojjonam <Manoj.jonam.boxer@gmail.com>
2022-05-06 10:08:09 +0530

    event date change

--- events/how-to-get-recruited-in-top-product-based-companies.php
+++ events/how-to-get-recruited-in-top-product-based-companies.php
@@ -12,7 +12,7 @@ include_once("../includes/header.php");
   <div class="container">
     <div class="row pt-5">
       <div class="col-xl-6 text-center text-md-start">
-        <h4 class="date-sec"><span>STARTS 06TH May 2022</span></h4>
+        <h4 class="date-sec"><span>STARTS 13TH May 2022</span></h4>
         <h1 class="top-head  pt-3">How to get recruited in Top Product Based Companies in 2022</h1>
         <div class="pricetag-strike" style="font-size:40px!important; font-weight:700 !important;">
           <del style="color:#888;font-size:14px;">₹ 1000</del>₹ Free
@@ -38,7 +38,7 @@ include_once("../includes/header.php");
             <div class="mini-boxevents w-100 shadow">
               <i class="fas fa-clock"></i>
               <h6 class="small-highlight pt-2">Date</h6>
-              <h3 class="mini-boxhead">06th May 2022</h3>
+              <h3 class="mini-boxhead">13th May 2022</h3>
             </div>
           </div>
           <div class="col-lg-2 col-sm-3 col-6 d-flex align-self-stretch mb-4">
@@ -522,7 +522,7 @@ if (document.getElementById('days')) {
     hour = minute * 60,
     day = hour * 24;
 
-  let countDown = new Date('May 06, 2022 19:00:00').getTime(),
+  let countDown = new Date('May 13, 2022 19:00:00').getTime(),
     x = setInterval(function() {
       let now = new Date().getTime(),
         distance = countDown - now;
