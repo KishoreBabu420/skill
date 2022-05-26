dc06b553e4ea023c5aa252627d2f15496f016c17
manojjonam <Manoj.jonam.boxer@gmail.com>
2022-05-26 12:15:44 +0530

    applied Ml, new event page added

---
+++ applied-machine-learning.php
@@ -0,0 +1,272 @@
+<?php 
+$title = "Workshop - Industry Trends and Recruitment Patterns in 2022 ";
+$description = "Join our LIVE webinar on how to stay with current emerging technologies to get the latest skills to get a secured job for tomorrow";
+$keywords = "Skill Safari, SkillSafari, skill safari, skillsafari, Skillsafari, skillSafari, Full Stack Developer Course, Data Science Boot Camp, Full Stack Developer, skillsafari.in, ";
+$page = 20;
+include_once("includes/header.php");
+?>
+<!-- top-space -->
+<section class="top-pagespace"></section>
+<!-- hire banner section started -->
+<section class="py-2 mt-2 text-white bg-hero">
+  <div class=" container py-3 px-md-5 rounded">
+    <div class="row align-items-center">
+      <div class="col-lg-7 text-md-start text-center">
+        <h1 class=" pt-3">India’s Best Beginner to Expert Course in Data Science</h1>
+        <p class="h5">Applied ML| Python | AI  [ certified by IABAC]</p>
+        <p class="hero-para text-white pt-3">Learn python from practical application. We combine theory with practical explanation to ensure you gain expertise in handling projects. Follow our instructors who have wide experience in explaining things clearly at a logical pace. Check out our huge catalogue of courses for more information</p>
+        <p>These contents are Far more valuable than the price you paid for We really hope you decide to purchase this course and take your knowledge to the next level. Let's get started.</p>
+        <p class="date-sec pt-3"><span><strong>Enroll now to join the Skill Safari community!<i class="fa-solid fa-circle-arrow-right m-2"></i></strong></span></h3>
+      </div>
+      <div class="col-lg-5 mt-4 mt-md-0">
+        <div class="training-form shadow mt-2 pt-md-4 pb-md-4 p-md-5 p-3">
+          <h5 class="trainingform-head py-3 text-center text-white">Have any Questions?</h5>
+          <form action="https://formsubmit.co/99a7386ade92b752097e7774d17f24fc" method="POST" id="lead-form">
+            <div class="mb-3">
+              <label for="fullName" class="form-label">Full Name*</label>
+              <input type="name" class="form-control" id="fullName" aria-describedby="fullNameHelp" name="name"
+                data-parsley-required-message="We require your name" placeholder="Enter Your Full Name" required />
+            </div>
+
+            <div class="mb-3">
+              <label for="email" class="form-label">Email Address*</label>
+              <input type="email" class="form-control" id="email" aria-describedby="emailHelp"
+                placeholder="Enter Your Email ID" name="email" data-parsley-required-message="Email Address is required"
+                required />
+            </div>
+
+            <div class="mb-3">
+              <label for="mobile" class="form-label">Mobile Number*</label>
+              <input type="tel" class="form-control" id="mobile" aria-describedby="mobileHelp"
+                placeholder="Enter Your Mobile Number" name="phone"
+                data-parsley-required-message="Mobile Number is required"
+                data-parsley-error-message="Provide valid Mobile Number" required data-parsley-trigger="keyup"
+                data-parsley-validation-threshold="1" data-parsley-debounce="500" data-parsley-type="digits"
+                data-parsley-minlength="10" data-parsley-maxlength="11" />
+            </div>
+            <input type="hidden" name="_webhook"
+              value="https://connect.pabbly.com/workflow/sendwebhookdata/IjM1MzIxMCI_3D">
+            <input type="hidden" name="_next" value="https://connect.skillsafari.in/dr7">
+            <input type="hidden" name="utm_source" value="">
+            <input type="hidden" name="utm_medium" value="">
+            <input type="hidden" name="utm_campaign" value="">
+            <input type="hidden" name="utm_term" value="">
+            <input type="hidden" name="utm_content" value="">
+            <div class="callbackbtn mt-3">
+              <button class="text-decoration-none submit-tag" type="submit">Request A Callback</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<!-- hiring process section started -->
+<section class="bg-studtrained">
+  <div class="container">
+    <div class="row pt-lg-5 pb-lg-5">
+      <div class="col-md-12 text-center pb-md-5 pb-4">
+        <h2 class="top-head pb-3">What You'll learn</h2>
+        <div class="what-will-list pt-4 pb-4">
+          <ul class="learn-list">
+            <li><span class="bg-shade">
+                <i class="fas fa-video"></i></span>
+              <span class="ms-3 cot"> what gets you hired in data science </span>
+            </li>
+            <li><span class="bg-shade">
+                <i class="far fa-clipboard"></i></span>
+              <span class="ms-3 cot">Hands-on Data Science Projects</span>
+            </li>
+            <li><span class="bg-shade">
+                <i class="fas fa-user-check"></i></span>
+              <span class="ms-3 cot">Real-time Application of Machine Learning</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-12 pb-md-5">
+        <div class="regbtn text-center">
+          <a href="" class="text-decoration-none" target="_blank">Register</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- hiring process section ended -->
+<section class="card p-3 my-2 border-0 rounded-3 pb-5 mb-5 my-4 advantage">
+    <div class="container">
+          <div class="section__title-container p-2">
+            <h2 class="section-title text-center"> Course features</h2>
+            <hr>
+          </div>
+          <div class="card-body p-3 my-2 border-0 rounded-3 advantage-card">
+            <h3 class="section__card-title">
+              <span><img
+                  src="https://res.cloudinary.com/skill-safari/image/upload/v1651854913/website/sales_icons/Uniq_1_bmezkp.png"
+                  alt="Quality Training" width="48" height="48"></span>Enroll enter without any previous knowledge
+            </h3>
+            <p class="section__card-description">
+            This course is completely designed for freshers in data science and ML. People with zero coding knowledge can take this opportunity to turn themselves into professional data scientists
+            </p>
+          </div>
+          <div class="card-body p-3 my-2 border-0 rounded-3 advantage-card">
+            <h3 class="section__card-title">
+              <span><img
+                  src="https://res.cloudinary.com/skill-safari/image/upload/v1651854913/website/sales_icons/uniq_2_uvrwuv.png"
+                  alt="ZERO Effective Cost " width="48" height="48"></span>The course is Filled with Guided tutorials with Hands-on Projects
+            </h3>
+            <p class="section__card-description">
+            Our lectures direct you to work on real-time projects to develop your skill level in realtime project handling
+            </p>
+          </div>
+          <div class="card-body p-3 my-2 border-0 rounded-3 advantage-card">
+            <h3 class="section__card-title">
+              <span><img
+                  src="https://res.cloudinary.com/skill-safari/image/upload/v1651854913/website/sales_icons/uniq_3_tjzulj.png"
+                  alt="Full Stack Developer Course Benefit" width="48" height="48"></span>Become an interview ready expert in Applied Machine Learning
+            </h3>
+            <p class="section__card-description">
+            Any beginner in the field of data science who takes this course will upgrade himself to the skill level of working professionals in the field
+            </p>
+          </div>
+          </div>
+    </section>
+
+    <section class="mt-5">
+        <div class="container">
+            <div class="row">
+            <div class="col-md-6 ps-5">
+        <div class="row align-items-center">
+          <h2 class="top-head pb-3">Key Takeaways</h2>
+
+          <div class="col-md-2 col-2 p-0">
+            <div class="stud-trainbox shadow text-center">
+              <i class="fa-solid fa-meteor"></i>
+            </div>
+          </div>
+          <div class="col-md-10 col-10">
+            <h6 class="timeline-head">IABAC certified</h6>
+            <p class="school-para">An only international organization to certify Data science. They follow rigorous assessment with european standards</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-2 col-2">
+            <div class="text-center pt-2 pb-2">
+              <img src="images/yellow-line.png" class="img-fluid" Loading="lazy" alt="full stack developer "
+                Loading="lazy" title="">
+            </div>
+          </div>
+        </div>
+        <!--  -->
+        <div class="row align-items-center">
+          <div class="col-md-2 col-2 p-0">
+            <div class="stud-trainbox shadow text-center">
+              <i class="fa-solid fa-meteor"></i>
+            </div>
+          </div>
+          <div class="col-md-10 col-10">
+            <h6 class="timeline-head">Trained by India’s best</h6>
+            <p class="school-para">HOur trainers are experienced in handling rough real life data refining and real world application projects for more than 10 years</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-2 col-2">
+            <div class="text-center pt-2 pb-2">
+              <img src="images/yellow-line.png" class="img-fluid" Loading="lazy" alt="fullstack event" title="">
+            </div>
+          </div>
+        </div>
+        <!--  -->
+        <div class="row align-items-center">
+          <div class="col-md-2 col-2 p-0">
+            <div class="stud-trainbox shadow text-center">
+              <i class="fa-solid fa-meteor"></i>
+            </div>
+          </div>
+          <div class="col-md-10 col-10">
+            <h6 class="timeline-head">Job Assistance </h6>
+            <p class="school-para">How to learn concepts of Trees, graphs and algorithms in DSA?</p>
+          </div>
+        </div>
+        
+        <!--  -->
+       
+
+
+      </div>
+      <div class="col-md-6 pt-3 pt-md-0">
+        <img src="https://res.cloudinary.com/skill-safari/image/upload/v1650940480/website/events/ab4_io0ral.jpg"
+          class="img-fluid w-100" Loading="lazy" alt="full stack development" title="">
+      </div>
+      <div class="col-md-12 mt-lg-5 pt-5 pb-md-5">
+        <div class="regbtn text-center">
+          <a href="https://meet.zoho.in/ozbjCzEaQz" target="_blank" class="text-decoration-none">Register</a>
+        </div>
+      </div>
+    </div>
+  </div>
+            </div>
+        </div>
+    </section>
+<!-- CERTIFICATION  -->
+<section class="bg-certified mb-5">
+  <div class="container">
+    <div class="row pt-5 pb-5">
+      <div class="col-md-12 pt-md-5 pb-5">
+        <h2 class="top-head text-center">Yes you will be certified with this program</h2>
+        <p class="top-para text-center ">Yes! You will be certified for this program once you submit your assignment.
+        </p>
+      </div>
+      <div class="col-md-7 pt-4">
+        <div class="certified-content">
+          <div class="row  mb-4">
+            <div class="col-lg-1 col-2">
+              <div class="s-ov"><i class="fas fa-shield-alt"></i></div>
+            </div>
+            <div class="col-lg-10 col-10">
+              <p class="cer-head w-100">IABAC certification </p>
+              <p class="school-para">This course will completely prepare you for facing one of the most valuable certifications in the field of analysis. IABAC [ International Association of Business Analytics certification ] is the only organization to certify data science professionals internationally</p>
+            </div>
+          </div>
+          <div class="row  mb-4">
+            <div class="col-lg-1 col-2">
+              <div class="s-ov"><i class="fas fa-share-alt"></i></div>
+            </div>
+            <div class="col-lg-10 col-10">
+              <p class="cer-head w-100">Global Opportunities</p>
+              <p class="school-para">IABAC certification opens global opportunities to you. Gaining to means your relevant job skills are recognized by companies across this world.</p>
+            </div>
+          </div>
+          <div class="row  mb-4">
+            <div class="col-lg-1 col-2">
+              <div class="s-ov"><i class="fas fa-lock"></i></div>
+            </div>
+            <div class="col-lg-10 col-10">
+              <p class="cer-head w-100">Higher Salaries</p>
+              <p class="school-para">On average, This certification opens up your opportunity to raise your pay potential by 30 to 40 %</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-5 pt-4">
+        <img
+          src="https://res.cloudinary.com/skill-safari/image/upload/v1650940481/website/events/certificate_qhfdhy.png"
+          class="img-fluid" alt="full stack pay after placement" Loading="lazy" title="">
+      </div>
+    </div>
+    <div class="row text-center">
+      <p class="school-para text-center pt-2 pb-3">It this excites you? Register Now</p>
+      <div class="regbtn">
+        <a href="https://meet.zoho.in/ozbjCzEaQz" class="text-decoration-none" target="_blank">Register Now</a>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+<?php 
+include_once("includes/footer.php");
+?>
