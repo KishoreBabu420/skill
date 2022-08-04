58ec23ba62a33cca7f0bc22e1d6638a050cc7cf4
kishore <kishore.sai37@gmail.com>
2022-08-04 14:40:32 +0530

    modal modified

--- sample.php
+++ sample.php
@@ -8,63 +8,83 @@ include_once("includes/header.php");
 <!-- top-space -->
 <section class="top-pagespace"></section>
 <section class="mt-3">
-    <div class="container">
-        <div class="shadow p-5 mb-5 bg-white rounded">
-            <div class="row">
-                <div class="col-2">01- Aug - 2022 </div>
-                <div class="col-2"><p>Mon-Fri</p>
-                                <p>Weekdays Regular</p></div>
-                <div class="col-4"><p>08:00 AM & 10:00 AM Batches</p>
-                                    <p>(Class 1Hr - 1:30Hrs) / Per Session</p></div>
-                <div class="col-2">        
-                      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#ModalCenter1">Learn More </button>
-                </div>
-                <!--modal  -->
-                <div class="modal fade" id="ModalCenter1" tabindex="-1" role="dialog" aria-labelledby="ModalCenter1Title" aria-hidden="true">
-                    <div class="modal-dialog modal-dialog-centered" role="document">
-                        <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="ModalCenter1Title">Modal title</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            ...
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
-                        </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- end of modal -->
-            </div>
-            <div class="row">
-                <div class="col-2">01- Aug - 2022 </div>
-                <div class="col-2"><p>Mon-Fri</p>
-                                <p>Weekdays Regular</p></div>
-                <div class="col-4"><p>08:00 AM & 10:00 AM Batches</p>
-                                    <p>(Class 1Hr - 1:30Hrs) / Per Session</p></div>
-                <div class="col-2">        
-                      <button><a href="full-stack-developer-course" class="text-decoration-none text-light">Learn More</a> </button>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-2">01- Aug - 2022 </div>
-                <div class="col-2"><p>Mon-Fri</p>
-                                <p>Weekdays Regular</p></div>
-                <div class="col-4"><p>08:00 AM & 10:00 AM Batches</p>
-                                    <p>(Class 1Hr - 1:30Hrs) / Per Session</p></div>
-                <div class="col-2">        
-                      <button><a href="full-stack-developer-course" class="text-decoration-none text-light">Learn More</a> </button>
-                </div>
-            </div>
+  <div class="container">
+    <div class="shadow p-5 mb-5 bg-white rounded">
+      <div class="row">
+        <div class="col-2">01- Aug - 2022 </div>
+        <div class="col-2">
+          <p>Mon-Fri</p>
+          <p>Weekdays Regular</p>
         </div>
+        <div class="col-4">
+          <p>08:00 AM & 10:00 AM Batches</p>
+          <p>(Class 1Hr - 1:30Hrs) / Per Session</p>
+        </div>
+        <div class="col-2">
+          <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#learnMore">
+            Learn More
+          </button>
+        </div>
+        <!-- Modal Start -->
+        <div class="modal fade" id="learnMore" tabindex="-1" aria-labelledby="learnMoreLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="learnMoreLabel">Modal title</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                ...
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Modal End -->
+
+      </div>
+      <div class="row">
+        <div class="col-2">01- Aug - 2022 </div>
+        <div class="col-2">
+          <p>Mon-Fri</p>
+          <p>Weekdays Regular</p>
+        </div>
+        <div class="col-4">
+          <p>08:00 AM & 10:00 AM Batches</p>
+          <p>(Class 1Hr - 1:30Hrs) / Per Session</p>
+        </div>
+        <div class="col-2">
+          <button><a href="full-stack-developer-course" class="text-decoration-none text-light">Learn More</a> </button>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-2">01- Aug - 2022 </div>
+        <div class="col-2">
+          <p>Mon-Fri</p>
+          <p>Weekdays Regular</p>
+        </div>
+        <div class="col-4">
+          <p>08:00 AM & 10:00 AM Batches</p>
+          <p>(Class 1Hr - 1:30Hrs) / Per Session</p>
+        </div>
+        <div class="col-2">
+          <button><a href="full-stack-developer-course" class="text-decoration-none text-light">Learn More</a> </button>
+        </div>
+      </div>
     </div>
+  </div>
 </section>
+
+
+<!-- Button trigger modal -->
+
+
+
+
 
 <?php 
 include_once("./includes/footer.php");
-?>  
+?>
