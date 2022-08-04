f00f3a1ad5b21f3e0b6e39191b3ba6f231bc256c
manojjonam <Manoj.jonam.boxer@gmail.com>
2022-08-04 11:31:12 +0530

    sample page

---
+++ sample.php
@@ -0,0 +1,7 @@
+<?php 
+$title = "Skill Safari - About Us";
+$description = "Skill Safari is an in demand E-learning platform. Know about us and our journey on how we came up with amazing products such as Full Stack Developer Program with job guarantee";
+$keywords = "Skill Safari, SkillSafari, skill safari, skillsafari, Skillsafari, skillSafari, Full Stack Developer Course, Data Science Boot Camp, Full Stack Developer, skillsafari.in, ";
+$page = 4;
+include_once("includes/header.php");
+?>
