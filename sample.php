3b8365dafba57959ece0b04a66c20c2be44eacd2
manojjonam <Manoj.jonam.boxer@gmail.com>
2022-08-04 12:39:33 +0530

    sample modal update

--- sample.php
+++ sample.php
@@ -17,7 +17,26 @@ include_once("includes/header.php");
                 <div class="col-4"><p>08:00 AM & 10:00 AM Batches</p>
                                     <p>(Class 1Hr - 1:30Hrs) / Per Session</p></div>
                 <div class="col-2">        
-                      <button><a href="full-stack-developer-course" class="text-decoration-none text-light">Learn More</a> </button>
+                      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter"><a href="" class="text-decoration-none text-light">Learn More</a> </button>
+                </div>
+                <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                    </div>
+                </div>
                 </div>
             </div>
             <div class="row">
