c571b88ce3124bbbe930f98bc7b4d01d34730246
manojjonam <Manoj.jonam.boxer@gmail.com>
2022-12-19 16:34:38 +0530

    spell check

--- events/roadmap_to_become_a_full_stack_developer.php
+++ events/roadmap_to_become_a_full_stack_developer.php
@@ -13,7 +13,7 @@ include_once("../includes/header.php");
     <div class="row pt-5">
       <div class="col-xl-6 text-center text-md-start">
         <p class="date-sec"><span>Coming Soon</span></p>
-        <h1 class="top-head  pt-3">Roadmap to become a Full stack ddeveloper </h1>
+        <h1 class="top-head  pt-3">Roadmap to become a Full stack developer </h1>
         <div class="pricetag-strike" style="font-size:40px!important; font-weight:700 !important;">
           <del style="color:#888;font-size:14px;">₹ 1000</del>₹ Free
         </div>
