7f0480bfd577a56dd3d1ecbb5b26733c5dc0f8af
Kishore <kishore.sai37@gmail.com>
2023-05-15 17:24:29 +0530

    add enquiry page

---
+++ learning-hub/enquiry.php
@@ -0,0 +1,282 @@
+<?php 
+
+$title = "Alchemyst 2.0 | Learn Mern Stack, Data Analytics, Automation testing in Coimbatore |  Online & Offline Training in Coimbatore
+";
+
+$description = "Alchemyst 2.0 offers multiple career tracks, such as MERN Stack, Data Analytics, and QA/Automation Testing in Coimbatore and help you start your tech career in an effective way.";
+
+$keywords = "Skill Safari, SkillSafari, skill safari, skillsafari, Skillsafari, skillSafari, Full Stack Developer Course, Data Analytics, Data Science, Automation Testing, Software Testing MERN Stack Developer, SOftware developer course in Coimbatore";
+
+$page = 15;
+
+include_once("../includes/header.php");
+?>
+
+<section class="top-pagespace"></section>
+<!-- top-space -->
+<!-- features tab -->
+<section class="text-white bg-hero">
+  <div class=" container py-3 px-md-5 rounded">
+    <div class="row align-items-center">
+      <div class="col-lg-7">
+        <div class="container">
+          <h1 class="hero-title text-lg-start text-center my-2">
+            Join with us to become a
+            <br />
+            <span class="intensive-courses change-text mb-3"></span>
+            <br />
+            <span class="mt-3 d-none d-md-block"> with Placement Assurance</span>
+          </h1>
+        </div>
+        <div class="accreditation-container container d-none d-md-block">
+          <div class="row align-items-center">
+            <div class="col-4">
+              <img src="https://res.cloudinary.com/skill-safari/image/upload/v1683791598/nasscom_d2dfl8_hfbyq0.webp"
+                alt="" class="nasscom-logo img-fluid">
+            </div>
+            <div class="col-4">
+              <img src="https://res.cloudinary.com/skill-safari/image/upload/v1683791598/IABAC_s6wllo.webp" alt=""
+                class="iabac-logo img-fluid">
+            </div>
+            <div class="col-4">
+              <img src="https://res.cloudinary.com/skill-safari/image/upload/v1683791597/skill_india_cenxif.webp" alt=""
+                class="skill-india-logo img-fluid">
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-5 mt-3 mt-md-0">
+        <div class="training-form shadow mt-2 pt-md-2 pb-md-4 p-md-5 p-3">
+          <h5 class="trainingform-head py-2 text-center text-white">Have any Questions?</h5>
+          <form action="https://formsubmit.co/99a7386ade92b752097e7774d17f24fc" method="POST" id="lead-form">
+            <div class="mb-2">
+              <label for="fullName" class="form-label">Full Name*</label>
+              <input type="name" class="form-control" id="fullName" aria-describedby="fullNameHelp" name="name"
+                data-parsley-required-message="We require your name" placeholder="Enter Your Full Name" required />
+            </div>
+
+            <div class="mb-2">
+              <label for="email" class="form-label">Email Address*</label>
+              <input type="email" class="form-control" id="email" aria-describedby="emailHelp"
+                placeholder="Enter Your Email ID" name="email" data-parsley-required-message="Email Address is required"
+                required />
+            </div>
+
+            <div class="mb-2">
+              <label for="mobile" class="form-label">Mobile Number*</label>
+              <input type="tel" class="form-control" id="mobile" aria-describedby="mobileHelp"
+                placeholder="Enter Your Mobile Number" name="phone"
+                data-parsley-required-message="Mobile Number is required"
+                data-parsley-error-message="Provide valid Mobile Number" required data-parsley-trigger="keyup"
+                data-parsley-validation-threshold="1" data-parsley-debounce="500" data-parsley-type="digits"
+                data-parsley-minlength="10" data-parsley-maxlength="11" />
+            </div>
+            <input type="hidden" name="_webhook"
+              value="https://connect.pabbly.com/workflow/sendwebhookdata/IjU3NmQwNTZmMDYzZjA0MzU1MjY0NTUzMCI_3D_pc">
+            <input type="hidden" name="_next" value="https://connect.skillsafari.in/dr7">
+            <input type="hidden" name="utm_source" value="">
+            <input type="hidden" name="utm_medium" value="">
+            <input type="hidden" name="utm_campaign" value="">
+            <input type="hidden" name="utm_term" value="">
+            <input type="hidden" name="utm_content" value="">
+            <div class="callbackbtn mt-3">
+              <button class="text-decoration-none submit-tag" type="submit">Request A Callback</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- features tab -->
+<!-- Career Tracks -->
+<section class="career-tracks mt-0 mb-3 py-3 advantage-card">
+  <div class="container">
+    <div class="section-title-container py-2 my-4 text-center ">
+      <h2 class="section-title section-title-two">Choose your Career Tracks</h2>
+    </div>
+    <div class="row align-items-center justify-between">
+      <div class="col-12 col-lg-4">
+        <div class="card bg-white  my-2 border-0 rounded-3 shadow">
+          <div class="card-header px-4 pt-3 track-header track-header-one">
+            <h3 class="track-card-title my-0">
+              MERN Stack Web Developer
+            </h3>
+            <p class="track-technologies my-2">Learn technologies : React JS, Node JS, Express JS, MongoDb</p>
+          </div>
+          <div class="card-body p-4 my-2">
+            <p class="track-card-description">
+              Become a Full Stack Developer with expertise on MERN Stack. Build a strong web development fundamentals
+              and problem solving skills. Develop web
+              applications using emerging frameworks and build a portfolio of projects.
+            </p>
+
+            <h4 class="track-card-title my-0">
+              Curriculum
+            </h4>
+            <ul class=" pt-3">
+              <li class="school-para pb-2">
+                UI & Version Control using HTML & CSS
+              </li>
+              <li class="school-para pb-2">
+                Adding Interactions and Functionalities
+              </li>
+              <li class="school-para pb-2">
+                Creating Single Page Applications (SPA's)
+              </li>
+              <li class="school-para">
+                Develop and Deploy Backend API's using Express and MongoDb
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-lg-4">
+        <div class="card bg-white  my-2 border-0 rounded-3 shadow">
+          <div class="card-header px-4 pt-3 track-header track-header-two">
+            <h3 class="track-card-title my-0">
+              Data Analyst
+            </h3>
+            <p class="track-technologies my-2">Learn technologies : Microsoft Excel, Numpy, Pandas, MatplotLib PowerBI
+            </p>
+          </div>
+          <div class="card-body p-4 my-2">
+            <p class="track-card-description">
+              Build a strong understanding of data analysis techniques, tools and methodologies. Become an expert in
+              uncovering insights and patterns from complex data sets. Create a portfolio of projects that describe your
+              abilities.
+            </p>
+
+            <h4 class="track-card-title my-0">
+              Curriculum
+            </h4>
+            <ul class=" pt-3">
+              <li class="school-para pb-2">
+                Data Analytics using Microsoft Excel
+              </li>
+              <li class="school-para pb-2">
+                Data Querying with SQL
+              </li>
+              <li class="school-para pb-2">
+                Data Cleaning & Pre Processing with Python
+              </li>
+              <li class="school-para">
+                Data Visualization and Story telling using Power BI
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-lg-4">
+        <div class="card bg-white  my-2 border-0 rounded-3 shadow">
+          <div class="card-header px-4 pt-3 track-header track-header-three">
+            <h3 class="track-card-title my-0">
+              QA/Automation Tester
+            </h3>
+            <p class="track-technologies my-2">Learn technologies : Core Java, Manual testing, Jenkins, Postman</p>
+          </div>
+          <div class="card-body p-4 my-2">
+            <p class="track-card-description">
+              Build your career as a Manual/Automation Test Engineer. Our industry-relevant curriculum ensures you
+              become an expert in identifying, tracking and reporting defects. Create a portfolio of projects that
+              describe your abilities.
+            </p>
+
+            <h4 class="track-card-title my-0">
+              Curriculum
+            </h4>
+            <ul class=" pt-3">
+              <li class="school-para pb-2">
+                Basics of Software testing & Manual Testing
+              </li>
+              <li class="school-para pb-2">
+                Programming & OOPS with Core Java
+              </li>
+              <li class="school-para pb-2">
+                Web and API Testing Postman
+              </li>
+              <li class="school-para">
+                Learn Continuous Integration with Jenkins and GIT
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-12 col-md-6 col-lg-4 m-auto">
+      <div class="callbackbtn mt-3">
+        <button type="button" data-bs-toggle="modal" data-bs-target="#partner-modal">Know
+          More</button>
+        <!-- Modal -->
+        <div class="modal fade" id="partner-modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-body">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="close"></button>
+                <div class="training-form bg-white shadow  pt-md-4 pb-md-4 p-md-5 p-3 text-start">
+                  <h5 class="trainingform-head pb-3">Want to know More?</h5>
+                  <form action="https://formsubmit.co/99a7386ade92b752097e7774d17f24fc" method="POST"
+                    id="cbe-partner-btn">
+                    <div class="mb-3">
+                      <label for="fullName" class="form-label">Full Name*</label>
+                      <input type="name" class="form-control" id="fullName" aria-describedby="fullNameHelp" name="name"
+                        data-parsley-required-message="We require your name" placeholder="Enter Your Full Name"
+                        required />
+                    </div>
+
+                    <div class="mb-3">
+                      <label for="email" class="form-label">Email Address*</label>
+                      <input type="email" class="form-control" id="email" aria-describedby="emailHelp"
+                        placeholder="Enter Your Email ID" name="email"
+                        data-parsley-required-message="Email Address is required" required />
+                    </div>
+
+                    <div class="mb-3">
+                      <label for="mobile" class="form-label">Mobile Number*</label>
+                      <input type="tel" class="form-control" id="mobile" aria-describedby="mobileHelp"
+                        placeholder="Enter Your Mobile Number" name="phone"
+                        data-parsley-required-message="Mobile Number is required"
+                        data-parsley-error-message="Provide valid Mobile Number" required data-parsley-trigger="keyup"
+                        data-parsley-validation-threshold="1" data-parsley-debounce="500" data-parsley-type="digits"
+                        data-parsley-minlength="10" data-parsley-maxlength="11" />
+                    </div>
+                    <input type="hidden" name="utm_source" value="">
+                    <input type="hidden" name="utm_medium" value="">
+                    <input type="hidden" name="utm_campaign" value="">
+                    <input type="hidden" name="utm_term" value="">
+                    <input type="hidden" name="utm_content" value="">
+                    <input type="hidden" name="_webhook"
+                      value="https://connect.pabbly.com/workflow/sendwebhookdata/IjU3NmQwNTZmMDYzZjA0MzU1MjY0NTUzMCI_3D_pc">
+                    <input type="hidden" name="_next" value="https://connect.skillsafari.in/dr7">
+                    <div class="callbackbtn mt-3">
+                      <button class="text-decoration-none" type="submit">Submit</button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- Career Tracks -->
+
+<?php 
+include_once("../includes/footer.php");
+?>
+
+<script type="text/javascript">
+//Home TYpe OBL,
+let typed = new Typed('.intensive-courses', {
+  strings: [
+    'Full Stack Developer',
+    'Data Analyst',
+    'QA Automation Tester'
+  ],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true,
+});
+</script>
