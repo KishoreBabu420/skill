144066886930b8713147244f758086742ef52e12
Kishore <kishore.sai37@gmail.com>
2023-07-19 18:03:13 +0530

    add registration form

--- registration_form.php
+++ registration_form.php
@@ -12,103 +12,43 @@ include_once("./includes/header.php");
   <div class="container py-5 h-100">
     <div class="row justify-content-center align-items-center h-100">
       <div class="col-12 col-lg-9 col-xl-7">
-        <div class="card shadow-2-strong card-registration" style="border-radius: 15px;">
-          <div class="card-body p-4 p-md-5">
-            <h3 class="mb-4 pb-2 pb-md-0 mb-md-5">Registration Form</h3>
-            <form>
+        <div class="training-form bg-white shadow  pt-md-4 pb-md-4 p-md-5 p-3">
+          <h5 class="trainingform-head pb-3">Register to know More?</h5>
+          <form action="https://formsubmit.co/99a7386ade92b752097e7774d17f24fc" method="POST" id="registration_page">
+            <div class="mb-3">
+              <label for="fullName" class="form-label">Full Name*</label>
+              <input type="name" class="form-control" id="fullName" aria-describedby="fullNameHelp" name="name"
+                data-parsley-required-message="We require your name" placeholder="Enter Your Full Name" required />
+            </div>
 
-              <div class="row">
-                <div class="col-md-6 mb-4">
+            <div class="mb-3">
+              <label for="email" class="form-label">Email Address*</label>
+              <input type="email" class="form-control" id="email" aria-describedby="emailHelp"
+                placeholder="Enter Your Email ID" name="email" data-parsley-required-message="Email Address is required"
+                required />
+            </div>
 
-                  <div class="form-outline">
-                    <input type="text" id="firstName" class="form-control form-control-lg" />
-                    <label class="form-label" for="firstName">First Name</label>
-                  </div>
-
-                </div>
-                <div class="col-md-6 mb-4">
-
-                  <div class="form-outline">
-                    <input type="text" id="lastName" class="form-control form-control-lg" />
-                    <label class="form-label" for="lastName">Last Name</label>
-                  </div>
-
-                </div>
-              </div>
-
-              <!-- <div class="row">
-                <div class="col-md-6 mb-4 d-flex align-items-center">
-
-                  <div class="form-outline datepicker w-100">
-                    <input type="text" class="form-control form-control-lg" id="birthdayDate" />
-                    <label for="birthdayDate" class="form-label">Birthday</label>
-                  </div>
-
-                </div>
-                <div class="col-md-6 mb-4">
-
-                  <h6 class="mb-2 pb-1">Gender: </h6>
-
-                  <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="femaleGender"
-                      value="option1" checked />
-                    <label class="form-check-label" for="femaleGender">Female</label>
-                  </div>
-
-                  <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="maleGender"
-                      value="option2" />
-                    <label class="form-check-label" for="maleGender">Male</label>
-                  </div>
-
-                  <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="otherGender"
-                      value="option3" />
-                    <label class="form-check-label" for="otherGender">Other</label>
-                  </div>
-
-                </div>
-              </div> -->
-
-              <div class="row">
-                <div class="col-md-6 mb-4 pb-2">
-
-                  <div class="form-outline">
-                    <input type="email" id="emailAddress" class="form-control form-control-lg" />
-                    <label class="form-label" for="emailAddress">Email</label>
-                  </div>
-
-                </div>
-                <div class="col-md-6 mb-4 pb-2">
-
-                  <div class="form-outline">
-                    <input type="tel" id="phoneNumber" class="form-control form-control-lg" />
-                    <label class="form-label" for="phoneNumber">Phone Number</label>
-                  </div>
-
-                </div>
-              </div>
-
-              <!-- <div class="row">
-                <div class="col-12">
-
-                  <select class="select form-control-lg">
-                    <option value="1" disabled>Choose option</option>
-                    <option value="2">Subject 1</option>
-                    <option value="3">Subject 2</option>
-                    <option value="4">Subject 3</option>
-                  </select>
-                  <label class="form-label select-label">Choose option</label>
-
-                </div>
-              </div> -->
-
-              <div class="mt-4 pt-2">
-                <input class="btn btn-primary btn-lg" type="submit" value="Submit" />
-              </div>
-
-            </form>
-          </div>
+            <div class="mb-3">
+              <label for="mobile" class="form-label">Mobile Number*</label>
+              <input type="tel" class="form-control" id="mobile" aria-describedby="mobileHelp"
+                placeholder="Enter Your Mobile Number" name="phone"
+                data-parsley-required-message="Mobile Number is required"
+                data-parsley-error-message="Provide valid Mobile Number" required data-parsley-trigger="keyup"
+                data-parsley-validation-threshold="1" data-parsley-debounce="500" data-parsley-type="digits"
+                data-parsley-minlength="10" data-parsley-maxlength="11" />
+            </div>
+            <input type="hidden" name="utm_source" value="">
+            <input type="hidden" name="utm_medium" value="">
+            <input type="hidden" name="utm_campaign" value="">
+            <input type="hidden" name="utm_term" value="">
+            <input type="hidden" name="utm_content" value="">
+            <input type="hidden" name="_webhook"
+              value="https://connect.pabbly.com/workflow/sendwebhookdata/IjU3NmQwNTZmMDYzZjA0MzU1MjY0NTUzMCI_3D_pc">
+            <input type="hidden" name="_next" value="https://connect.skillsafari.in/dr7">
+            <div class="callbackbtn mt-3">
+              <button class="text-decoration-none" type="submit">Submit</button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
