f29d5db30a2d87a995e2bded6bcb789da3f8a337
Manoj <manojjonam@gmail.com>
2023-08-25 14:55:04 +0530

    DSML page

---
+++ data-science-machine-learning/data-science-machine-learning-experts.php
@@ -0,0 +1,213 @@
+<?php 
+
+$title = "Alchemyst 2.0 | Learn Mern Stack, Data Analytics, Automation testing in Coimbatore |  Online & Offline Training in Coimbatore
+";
+
+$description = "Alchemyst 2.0 offers multiple career tracks, such as MERN Stack, Data Analytics, and QA/Automation Testing in Coimbatore and help you start your tech career in an effective way.";
+
+$keywords = "Skill Safari, SkillSafari, skill safari, skillsafari, Skillsafari, skillSafari, Full Stack Developer Course, Data Analytics, Data Science, Automation Testing, Software Testing MERN Stack Developer, SOftware developer course in Coimbatore";
+
+$page = 15;
+
+include_once("../includes/header.php");
+?>
+
+<section class="top-pagespace"></section>
+<!-- top-space -->
+<section class="text-white bg-hero">
+  <div class=" container py-3 px-md-5 rounded">
+    <div class="row align-items-center">
+      <div class="col-lg-7">
+        <div class="container">
+          <h1 class="hero-title text-lg-start text-center my-2">
+            Data science & Machine Learning Course
+            with Assured Placement
+          </h1>
+          <div class="py-3">
+            <p class="date-sec pt-3"><span><strong>Next Batch Starts:</strong> August 21<sup>th</sup> 2023</span></h3>
+          </div>
+          <div class="py-3">
+            <p class=" pt-3 h3"><i class="fa-solid fa-location-dot alchemyst-location-icon"></i> Coimbatore</h3>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-5 mt-3 mt-md-0">
+        <div class="training-form shadow mt-2 pt-md-2 pb-md-4 p-md-5 p-3">
+          <h5 class="trainingform-head py-2 text-center text-white">Request A Callback</h5>
+          <form action="https://formsubmit.co/99a7386ade92b752097e7774d17f24fc" method="POST" id="lead-form">
+            <div class="mb-2">
+              <label for="fullName" class="form-label">Full Name*</label>
+              <input type="name" class="form-control" id="fullName" aria-describedby="fullNameHelp" name="name"
+                data-parsley-required-message="We require your name" placeholder="Enter Your Full Name" required />
+            </div>
+
+            <div class="mb-2">
+              <label for="email" class="form-label">Email Address*</label>
+              <input type="email" class="form-control" id="email" aria-describedby="emailHelp"
+                placeholder="Enter Your Email ID" name="email" data-parsley-required-message="Email Address is required"
+                required />
+            </div>
+
+            <div class="mb-2">
+              <label for="mobile" class="form-label">Mobile Number*</label>
+              <input type="tel" class="form-control" id="mobile" aria-describedby="mobileHelp"
+                placeholder="Enter Your Mobile Number" name="phone"
+                data-parsley-required-message="Mobile Number is required"
+                data-parsley-error-message="Provide valid Mobile Number" required data-parsley-trigger="keyup"
+                data-parsley-validation-threshold="1" data-parsley-debounce="500" data-parsley-type="digits"
+                data-parsley-minlength="10" data-parsley-maxlength="11" />
+            </div>
+            <input type="hidden" name="_webhook"
+              value="https://connect.pabbly.com/workflow/sendwebhookdata/IjU3NjUwNTZjMDYzNTA0MzM1MjZlNTUzYzUxMzIi_pc">
+            <input type="hidden" name="_next" value="https://connect.skillsafari.in/dr7">
+            <input type="hidden" name="utm_source" value="">
+            <input type="hidden" name="utm_medium" value="">
+            <input type="hidden" name="utm_campaign" value="">
+            <input type="hidden" name="utm_term" value="">
+            <input type="hidden" name="utm_content" value="">
+            <div class="callbackbtn mt-3">
+              <button class="text-decoration-none submit-tag" type="submit">Submit</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="bg-smallicon-sec active">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 pt-4 pb-2 text-center">
+        <h2 class="text-center top-head">We are accredited by</h2>
+      </div>
+    </div>
+    <div class="row align-items-center pt-md-3 justify-content-center">
+      <div class="col-md-3 col-6 mb-4">
+        <div class="iconbox-small text-center">
+          <div class="icon-container">
+            <img
+              src="https://res.cloudinary.com/skill-safari/image/upload/v1688626100/website/web%20images/nasscom_ivm0o1.webp"
+              alt="100% Placement Guarantee" class="img-fluid">
+          </div>
+
+        </div>
+      </div>
+      <div class="col-md-3 col-6 mb-4">
+        <div class="iconbox-small text-center">
+          <div class="icon-container">
+            <img
+              src="https://res.cloudinary.com/skill-safari/image/upload/v1688626100/website/web%20images/iabac_lsk3ij.webp"
+              alt="7 Months Live Course" class="img-fluid">
+          </div>
+
+        </div>
+      </div>
+      <div class="col-md-3 col-6 mb-4">
+        <div class="iconbox-small text-center">
+          <div class="icon-container">
+            <img
+              src="https://res.cloudinary.com/skill-safari/image/upload/v1688626100/website/web%20images/skill_india_nff8zb.webp"
+              alt="1 - 1 Mentorship support" class="img-fluid">
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section>
+  <div class="container">
+    <h2 class="text-center mt-5 mb-3">Data Science and Machine Learning Experts Course Fees</h2>
+    <div class="row justify-content-around mt-5">
+      <div class="col col-lg-4 ">
+        <div class="card pricing-card-Dsml bg-hero text-white">
+          <div class="card-body">
+            <h5 class="card-title text-center h3 mb-3 ">Online Program Fees</h5>
+            <p class="h2 text-center mb-3"><del><small>Rs 80,000/-</small></del></br> ₹40,000 + GST</p>
+            <ul>
+              <li class=" mb-3">EMI options are available with 0% interest</li>
+              <hr>
+              <li class="mb-3 p">3,6,9 Months based on CIBIL Score </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col col-lg-4">
+
+        <div class="card pricing-card-Dsml bg-hero text-white">
+          <div class="card-body">
+            <h5 class="card-title text-center h3 mb-3 ">Offline Program Fees</h5>
+            <p class="h2 text-center mb-3"><del><small>Rs 1,30,000/-</small></del> </br>₹65,000 + GST</p>
+            <ul>
+              <li class=" mb-3">EMI options are available with 0% interest</li>
+              <hr>
+              <li class="mb-3 p">3,6,9 Months based on CIBIL Score </li>
+            </ul>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</section>
+<!-- Footer CTA START -->
+<section class="bg-callback mt-md-4">
+  <div class="container">
+    <div class="row  pt-5 pb-5">
+      <div class="col-lg-12 text-center">
+        <h2 class="text-white top-head">Want to know more about Alchemyst 2.0?</h2>
+        <p class="para-top text-white text-center"> Want to know
+          how you can join the cohort? or if you have any questions, feel free to connect with our career counsellors.
+        </p>
+        <div class="row align-items-center justify-content-center pt-4">
+          <div class="col-lg-3 col-sm-6 mb-4 mb-md-0 p-0">
+            <div class="hirebtn-1 text-center">
+              <a href="tel:+918111099100" class="text-decoration-none">Call Us</a>
+            </div>
+          </div>
+          <div class="col-lg-3 col-sm-6 mb-4 mb-md-0 lastsec">
+            <div class="whatsappbtn text-center">
+              <a target="_blank" href="https://api.whatsapp.com/send?phone=+918111099100"
+                class="text-white text-decoration-none">Whatsapp Us</a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+  </div>
+</section>
+<!-- Footer CTA End -->
+<!-- Certification -->
+<section class="certification my-3 py-3">
+  <div class="container">
+    <div class="section-title-container py-2 my-4 text-center ">
+      <h2 class="section-title section-title-two">Get Certified</h2>
+    </div>
+    <div class="row align-items-center justify-content-around">
+      <div class="col-md-6 col-12">
+        <div class="certificate-description">
+          <h4 class="text-center text-md-start">
+            You will be given a certificate proving your industry readiness after submitting your projects and passing
+            the pre-placement test.
+          </h4>
+        </div>
+      </div>
+      <div class="col-md-6 col-12 ">
+        <div class="certificate__img-container mx-auto">
+          <img
+            src="https://res.cloudinary.com/skill-safari/image/upload/v1690278539/alchemyst-2.o-certificate_h1ayx1.jpg"
+            alt="Skill Safari certificate" class="img-fluid">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- Certification -->
+<?php 
+include_once("../includes/footer.php");
+?>
+
+<script type="text/javascript">
