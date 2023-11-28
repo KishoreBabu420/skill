2bd66148e92160877e3635c0795960c071f71f14
manojjonam <manoj.jonam.boxer@gmail.com>
2023-11-28 15:55:18 +0530

    update

--- includes/footer.php
+++ includes/footer.php
@@ -115,13 +115,13 @@
 
 <!-- footer section ended -->
 <!-- bootstrap -->
-<script src="<?php echo $bootstrapJS; ?>"></script>
+<script async src="<?php echo $bootstrapJS; ?>"></script>
 <!-- carousel -->
 <script async src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<script src="<?php echo $owlCarouselJS; ?>" type="text/javascript"></script>
-<script src="<?php echo $parsleyJS; ?>"></script>
-<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
-<script src="<?php echo $customJS; ?>"></script>
+<script async src="<?php echo $owlCarouselJS; ?>" type="text/javascript"></script>
+<script async src="<?php echo $parsleyJS; ?>"></script>
+<script async src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
+<script async src="<?php echo $customJS; ?>"></script>
 
 <!-- Linkedin insight tag -->
 <script type="text/javascript">
